befc4d67d24a24890d3de5c4d976a8214cf1e352
Amjadali168 <amjadali1682002@gmail.com>
2024-06-02 00:54:56 +0530

    login

--- ecommerce/authcart/views.py
+++ ecommerce/authcart/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth.models import User
+from django.views.generic import View
 from django.contrib import messages
 from django.template.loader import render_to_string
-from django.utils.http import urlsafe_base64_encode
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from .utils import generate_token, TokenGenerator
-from django.utils.encoding import force_bytes
+from django.utils.encoding import force_bytes, force_text
+from django.core.mail import EmailMessage
+from django.conf import settings
 
 # Create your views here.
 def signup(request):
@@ -23,6 +26,7 @@ def signup(request):
             pass
 
         user = User.objects.create_user(email, email, password)
+        user.is_active= False
         user.save()
         email_subject = "Activate Your Account"
         message = render_to_string('activate.html',{
@@ -31,8 +35,35 @@ def signup(request):
             'uid':urlsafe_base64_encode(force_bytes(user.pk)),
             'token':generate_token.make_token(user)
         })
-        return HttpResponse("User Created!!", email)
+        email_message = EmailMessage(email_subject,message,settings.EMAIL_HOST_USER,[email])
+        # email_message.send()
+        messages.success(request,"Activate your account by clicking the link in your given emails")
+
+
+
+        # return HttpResponse("User Created!!", email)
     return render(request, "signup.html")
+
+
+
+class ActivateAccountView(View):
+    def get(self, request, uidb64, token):
+        try:
+            uid = force_text(urlsafe_base64_decode(uidb64))
+            user = User.objects.get(pk=uid)
+        except Exception as identifier:
+            user= None
+        if user is not None and generate_token.check_token(user,token):
+            user.is_active= True
+            user.save()
+            messages.info(request,"Account activated successfully")
+            return redirect('/auth/login')
+        return render(request,'auth/activatefail.html')
+
+
+
+
+
 
 def handlelogin(request):
     return render(request, "login.html")
